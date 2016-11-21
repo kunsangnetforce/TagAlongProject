4857e7b9384264a34d138e80b47dc561e055b503
tenzinnetforce <tenzin@netforceinfotech.com>
2016-11-21 11:53:07 +0530

    login and signup

--- app/src/main/java/com/netforceinfotech/tagalong/Login/ForgotPasswordActivity.java
+++ app/src/main/java/com/netforceinfotech/tagalong/Login/ForgotPasswordActivity.java
@@ -7,24 +7,22 @@ import android.support.v7.widget.Toolbar;
 
 import com.netforceinfotech.tagalong.R;
 
-public class SignUpActivity extends AppCompatActivity {
-
+public class ForgotPasswordActivity extends AppCompatActivity {
     private Toolbar toolbar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_forgot_password);
 
         InitVal();
-
     }
 
     private void InitVal() {
 
         toolbar= (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
--- app/src/main/java/com/netforceinfotech/tagalong/Login/LoginActivity.java
+++ app/src/main/java/com/netforceinfotech/tagalong/Login/LoginActivity.java
@@ -13,6 +13,7 @@ import com.netforceinfotech.tagalong.R;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView signupTextView;
+    private TextView forgotPasswordtextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void InitVal() {
 
         signupTextView = (TextView) findViewById(R.id.signupTextView);
-
+        forgotPasswordtextView= (TextView) findViewById(R.id.forgotPasswordtextView);
         signupTextView.setOnClickListener(this);
         signupTextView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
 
@@ -40,6 +41,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             overridePendingTransition(R.anim.enter, R.anim.exit);
             startActivity(intent);
 
+        }if(v.getId()==R.id.forgotPasswordtextView){
+
+            Intent intent = new Intent(LoginActivity.this,ForgotPasswordActivity.class);
+            overridePendingTransition(R.anim.enter, R.anim.exit);
+            startActivity(intent);
         }
 
     }
