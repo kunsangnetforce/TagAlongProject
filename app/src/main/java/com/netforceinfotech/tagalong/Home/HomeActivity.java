17e0e8cd82c26cd4251f9e0165eb9a1d6e89275b
tenzinnetforce <tenzin@netforceinfotech.com>
2016-11-21 14:24:44 +0530

    login and signup

---
+++ app/src/main/java/com/netforceinfotech/tagalong/Home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.netforceinfotech.tagalong.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.netforceinfotech.tagalong.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/netforceinfotech/tagalong/Login/LoginActivity.java
+++ app/src/main/java/com/netforceinfotech/tagalong/Login/LoginActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.netforceinfotech.tagalong.R;
 
@@ -14,6 +16,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private TextView signupTextView;
     private TextView forgotPasswordtextView;
+    private Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +27,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void InitVal() {
-
+        loginButton = (Button) findViewById(R.id.loginButton);
         signupTextView = (TextView) findViewById(R.id.signupTextView);
         forgotPasswordtextView= (TextView) findViewById(R.id.forgotPasswordtextView);
         signupTextView.setOnClickListener(this);
+        forgotPasswordtextView.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
         signupTextView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
 
 
@@ -46,6 +52,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LoginActivity.this,ForgotPasswordActivity.class);
             overridePendingTransition(R.anim.enter, R.anim.exit);
             startActivity(intent);
+        }if(v.getId()==R.id.loginButton){
+
+            Toast.makeText(getApplicationContext(),"Login Button Clicked",Toast.LENGTH_LONG).show();
+        }if(v.getId()==R.id.facebookLayout){
+
+            Toast.makeText(getApplicationContext(),"FAcebook",Toast.LENGTH_LONG).show();
         }
 
     }
